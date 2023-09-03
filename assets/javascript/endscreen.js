4ae11b2fa946da871cc2d789d656abe20efa572f
LukaB25 <>
2023-09-03 16:06:49 +0000

    Add changes and created separate js files for highscores and endscreen

--- assets/javascript/endscreen.js
+++ assets/javascript/endscreen.js
@@ -5,8 +5,9 @@ const saveButton = document.getElementById('save-score-btn');
 const newestScore = localStorage.getItem('newestScore');
 const finalScore = document.getElementById('final-score-total');
 
-const highScores = JSON.parse(localStorage.getItem('highScores'));
-console.log(highScores);
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+const maxHighScores = 5;
 
 finalScore.innerText = newestScore;
 
@@ -14,12 +15,31 @@ username.addEventListener('keyup', () => {
     saveButton.disabled = !username.value;
 });
 
+// Code for submit on Enter was used from: https://weekendprojects.dev/posts/addeventlistener-for-enter-key/?utm_content=cmp-true
+username.addEventListener('keydown', function (event) {
+    if (event.keyCode === 13 || event.key === 'Enter') {
+        console.log('Enter key was pressed');
+    }
+});
+
 saveHighScore = (e) => {
     console.log('Clicked save button.');
     e.preventDefault();
 
-    const score =  {
+    const score = {
         score: newestScore,
         name: username.value
     };
+
+    highScores.push(score);
+
+    highScores.sort((a, b) => b.score - a.score);
+
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+
+    console.log(score);
+
+    return window.location.assign('/highscores.html');
 };
---
+++ assets/javascript/highscores.js
@@ -0,0 +1,10 @@
+// Display High Scores
+
+const highScoresList = document.getElementById('high-score-list');
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+highScoresList.innerHTML = highScores.map(score => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+}).join('');
+
+
